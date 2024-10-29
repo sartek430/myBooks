b087b3758b670d63ddc2ddc5f59d600a5acfde4b
clement-jny <clementjaunay@free.fr>
2024-10-29 23:46:47 +0100

    feat: create basic Input, Button and Icons components for reusability, export files

---
+++ components/common/Icons.tsx
@@ -0,0 +1,5 @@
+import { AntDesign, EvilIcons } from "@expo/vector-icons";
+
+const Icons = { AntDesign, EvilIcons };
+
+export default Icons;
---
+++ components/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from "./common/Button";
+export { default as Icons } from "./common/Icons";
+export { default as Input } from "./common/Input";
