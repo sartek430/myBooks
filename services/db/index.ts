5f682f71203c4522f6ebe6f89e4e07d0da2d82c2
clement-jny <clementjaunay@free.fr>
2024-10-31 00:54:29 +0100

    feat: create all firestore collections with their methods | update index

--- services/db/index.ts
+++ services/db/index.ts
@@ -1,3 +1,4 @@
-// export * as book from './book'
-export * as user from './user'
-// export * as userBooks from './userBooks'
+export { default as book } from './book'
+export { default as comment } from './comment'
+export { default as userBooks } from './userBooks'
+export { default as user } from './user'
